4fe116feefcb27188cac5b045a931856749a2079
daniel-oguejiofor <d.oguejiofor@maontech.com>
2025-09-25 04:48:21 +0100

    deleted the hue.js file

--- hue.js
+++
@@ -1,8 +0,0 @@
-const { slugify, capitalize, camelCase, titleCase } = require(".");
-
-const sentence = "My name is Daniel and I'm a child of God.";
-
-console.log("Slugified: ", slugify(sentence));
-console.log("capitalized: ", capitalize(sentence));
-console.log("camel cased: ", camelCase(sentence));
-console.log("title cased: ", titleCase(sentence));
